6df8d4d84238bab45307d6f1cea0696e8becab9e
dxb99 <dxb99.clan@gmail.com>
2026-03-19 03:48:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -652,15 +652,12 @@ async function savePlayers(){
     const name = row.cells[0].innerText.trim();
     const skill = parseInt(row.cells[1].innerText.trim());
 
-    /* 🔥 FORCE CLEAN NAME FORMAT */
-    const cleanName = name.replace(/\s+/g, " ").trim();
+    if(!name) return;
 
-    if(!cleanName) return;
-
-  players.push({
-  name: cleanName,
-  skill: skill
-});
+    players.push({
+      name:name,
+      skill:skill
+    });
 
   });
 
