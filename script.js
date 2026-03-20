e5072175c97db5248351b93e4377d463475db36c
dxb99 <dxb99.clan@gmail.com>
2026-03-20 00:46:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+alert("NEW SCRIPT LOADED");
+
 const API_URL = "https://script.google.com/macros/s/AKfycbzIyBeXAVeSLtxW8jR9OnQL_Iz6cawGiaZSlkoZ2hTYy5dwo-0n_GH6F15H7tfXojIl/exec";
 
 let allPlayers = [];
