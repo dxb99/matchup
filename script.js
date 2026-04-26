aaab1afc169a126419a2561215a3ccab63a68541
dxb99 <dxb99.clan@gmail.com>
2026-04-26 16:32:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -292,6 +292,9 @@ async function api(data){
 
   const res = await fetch(API_URL,{
     method:"POST",
+    headers:{
+      "Content-Type":"text/plain;charset=utf-8"
+    },
     body:JSON.stringify(data)
   });
 
